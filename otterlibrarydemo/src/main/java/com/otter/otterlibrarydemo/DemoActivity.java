4b83b6b6009dcce374b1df3c2ec97debcd87490b
samuel22gj <samuel22gj@gmail.com>
2015-05-18 17:59:27 +0800

    [DemoApp] Add new menu item to clear log view

--- otterlibrarydemo/src/main/java/com/otter/otterlibrarydemo/DemoActivity.java
+++ otterlibrarydemo/src/main/java/com/otter/otterlibrarydemo/DemoActivity.java
@@ -163,9 +163,12 @@ public abstract class DemoActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_clear_log:
+                log.setText("");
+                break;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
