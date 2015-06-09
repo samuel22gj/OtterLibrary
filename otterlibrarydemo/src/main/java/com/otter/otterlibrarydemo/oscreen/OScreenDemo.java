f1dae5ef4ee8351fcd219c0c0df297a8400fd0e8
samuel22gj <samuel22gj@gmail.com>
2015-06-09 14:28:01 +0800

    [DemoApp] Add OScreen demo

---
+++ otterlibrarydemo/src/main/java/com/otter/otterlibrarydemo/oscreen/OScreenDemo.java
@@ -0,0 +1,53 @@
+package com.otter.otterlibrarydemo.oscreen;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.otter.otterlibrary.OScreen;
+import com.otter.otterlibrarydemo.DemoActivity;
+
+public class OScreenDemo extends DemoActivity {
+    private static final String BILLBOARD =
+            "Press below buttons to get screen width, height and density.";
+    private static final String[] OPERATION_ITEM = {
+            "getWidth", "getHeight",
+            "getRealWidth", "getRealHeight",
+            "getDensity", "getDensityDpi"
+    };
+
+    @Override
+    public String setBillboard() {
+        return BILLBOARD;
+    }
+
+    @Override
+    public String[] setOperationItem() {
+        return OPERATION_ITEM;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0: // getWidth
+                appendLog("getWidth: " + OScreen.getWidth(getApplicationContext()));
+                break;
+            case 1: // getHeight
+                appendLog("getHeight: " + OScreen.getHeight(getApplicationContext()));
+                break;
+            case 2: // getRealWidth
+                appendLog("getRealWidth: " + OScreen.getRealWidth(getApplicationContext()));
+                break;
+            case 3: // getRealHeight
+                appendLog("getRealHeight: " + OScreen.getRealHeight(getApplicationContext()));
+                break;
+            case 4: // getDensity
+                appendLog("getDensity: " + OScreen.getDensity(getApplicationContext()));
+                break;
+            case 5: // getDensityDpi
+                appendLog("getDensityDpi: " + OScreen.getDensityDpi(getApplicationContext()));
+                break;
+            default:
+                break;
+        }
+    }
+}
